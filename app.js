4e871b019f31fd6539a9d05c95c7b470a323e5b4
Rohan <imgr8rohan6@gmail.com>
2021-07-13 20:44:05 +0530

    second upgradaion

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 var md5 = require('md5');
+const Swal = require('sweetalert2')
 mongoose.connect("mongodb://localhost:27017/questionDB", { useUnifiedTopology: true , useNewUrlParser: true }) //mongoose connecting
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set('views', './views');
 app.set('view engine', 'ejs')
+
+
 
 questionSchema = {text:String,photo:String,answer:String,format:String}
 Question = mongoose.model('Question',questionSchema)
@@ -19,7 +22,7 @@ app.get("/34ec78fcc91ffb1e54cd85e4a0924332",function(req, res){
 })
 
 app.post("/34ec78fcc91ffb1e54cd85e4a0924332",function(req, res){
-    const question = new Question({text:req.body.text,photo:req.body.photo,answer:req.body.answer,format:req.body.age})
+    const question = new Question({text:req.body.text,photo:req.body.photo,answer:req.body.answer,format:req.body.age,index:req.body.index})
     question.save(function(err){
         if(!err){
             console.log("pushed")
@@ -38,47 +41,47 @@ app.get("/34ec78fcc91ffb1e54cd85e4a0924332/view",function(req, res){
 })
 
 
-
 app.post("/delete",function(req, res){
-var delID=req.body.delete
-Question.deleteOne({_id:delID},function(err){  //id vech finding then deleting
-    if(!err){
-        console.log("popped")
-     return res.redirect("/34ec78fcc91ffb1e54cd85e4a0924332/view")
-   }
- })
-})
+    var delID=req.body.delete
+    Question.deleteOne({_id:delID},function(err){  //id vech finding then deleting
+        if(!err){
+            console.log("popped")
+         return res.redirect("/34ec78fcc91ffb1e54cd85e4a0924332/view")
+       }
+     })
+    })
 
-app.get("/", (req, res) =>{ 
-    res.render("vali",{message:"welcome",link:"/question/"+md5(0)})
-})
-
-
+var score = 0
+    app.get("/",function(req, res){
 Question.find({},function(err,results){
-     results.forEach(function(result,index){
-        app.get("/question/"+md5(index),function(req, res){
-            res.render("index",{text:result.text,photo:result.photo,format:result.format,code:md5(index)})
-        })
-        app.post("/question/post/"+md5(index),function(req, res){
-            if(_.lowerCase(req.body.answer)==_.lowerCase(result.answer)){
-                res.render("vali",{message:"congrats Next🎉👌✨",link:"/question/"+md5(index+1)})
-            }
-            else{
-                res.render("vali",{message:"sorry😢😢",link:"/question/"+md5(index)})
-            }
-        })
+    if(score!=results.length){
+      res.render("index",{text:results[score].text,photo:results[score].photo,format:results[score].format})
+    }
+    else{
+        res.render("vali",{message:"your treasure",link:"/treasure"})
+        score = 0
+    }
     })
-    app.get("/question/"+md5(results.length),function(req, res){
-        res.render("vali",{message:"YAY you've cracked it👏👏🎉👌👌🎉🎉🎊 click here",link:"/treasure"})
-    })
-   
 })
+    app.post("/",function(req, res){
+        Question.find({},function(err,results){
+        if(req.body.answer===results[score].answer){
+            console.log("yay")
+            score+=1
+            res.render("vali",{message:"Good Job",link:"/"})
+        }
+        else{
+            res.render("vali",{message:"Try Again",link:"/"})
+
+        
+    }
+    })
+})
+
 
 app.get("/treasure",function(req, res){
     res.render("treasure")
 })
-
-
 
 app.listen(3000, function(){
     console.log("listening")
